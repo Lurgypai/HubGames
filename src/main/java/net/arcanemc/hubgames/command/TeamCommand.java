029231470be5fb4009f396d1eccdf4af935e076c
BuildTools <unconfigured@null.spigotmc.org>
2017-10-09 16:41:12 -0700

    commit

--- src/main/java/net/arcanemc/hubgames/command/TeamCommand.java
+++ src/main/java/net/arcanemc/hubgames/command/TeamCommand.java
@@ -2,18 +2,13 @@ package net.arcanemc.hubgames.command;
 
 import org.bukkit.entity.Player;
 
-import net.arcanemc.core.api.commands.Command;
-import net.arcanemc.core.api.commands.CommandBase;
+import net.arcanemc.core.api.commands.nCommands.Command;
+import net.arcanemc.core.api.commands.nCommands.Inject;
 import net.arcanemc.hubgames.HubMinigames;
 
-public class TeamCommand extends CommandBase {
-	
-	
-	
-	@Command(name = "team", playerOnly = true)
-	public void onTeamCommand(Player player, String[] args) {
-			if(args.length == 1) {
-				HubMinigames.getInstance().getGameManager().joinPlayerToGame(player.getUniqueId(), args[0]);
-			}
+public class TeamCommand {
+	@Command(names = { "team" })
+	public void onTeamCommand(@Inject(data = Inject.Data.SENDER, nullable = false)Player player, @Inject(nullable = true)String args) {
+				HubMinigames.getInstance().getGameManager().joinPlayerToGame(player.getUniqueId(), args);
 	}
 }
